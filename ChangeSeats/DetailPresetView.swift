1f81639d0ebeb732a609cb7a671dd20bf7d17450
Rin <rin.t.4921@gmail.com>
2020-09-19 22:15:22 +0900

    テスト練習用

--- ChangeSeats/DetailPresetView.swift
+++ ChangeSeats/DetailPresetView.swift
@@ -16,27 +16,38 @@ class DetailPresetViewModel: ObservableObject {
         self.state = state
     }
     
+    func change(uuid: UUID, number: Int) {
+        for columnIndex in 0..<columnSeats.count {
+            for rowIndex in 0..<columnSeats[columnIndex].rowSeats.count {
+                if columnSeats[columnIndex].rowSeats[rowIndex].id == uuid {
+                    columnSeats[columnIndex].rowSeats[rowIndex].number = number
+                }
+            }
+        }
+    }
+    
 }
 
 
 struct DetailPresetView: View {
-    //var id: UUID
-    //@ObservedObject var viewModel: DetailPresetViewModel
-    //@State var number: Int = 9
+    @State var id: UUID
+    @ObservedObject var viewModel: DetailPresetViewModel
+    @State var number: Int = 1
+    var studentNumber: Int
     
-//    init(columnSeats: [ColumnSeats], state: StudentState) {
-//        viewModel = DetailPresetViewModel(columnSeats: columnSeats, state: state)
-//    }
     
+
     var body: some View {
         VStack{
             Text("この席に座る人の番号を選択")
-//            Picker(selection: $number, label: Text(" ")) {
-//                
-//            }
-
-        }.onAppear{
-            //print(self.id)
+          Picker(selection: $number, label: Text("")) {
+                ForEach(1..<studentNumber + 1, id: \.self) { index in
+                    Text(String(index))
+                }
+            }
+            .frame(width: 200)
+        }.onDisappear{
+            self.viewModel.change(uuid: self.id, number: self.number)
         }
     }
 }
--- ChangeSeats/PresetView.swift
+++ ChangeSeats/PresetView.swift
@@ -19,10 +19,15 @@ class PresetViewModel: ObservableObject {
     
 }
 
+class PresetNumber: ObservableObject {
+    @Published var number: Int = 1
+}
+
 
 struct PresetView: View {
     
     @ObservedObject var viewModel: PresetViewModel
+    @State var isModal: Bool = false
     
     init(columnSeats: [ColumnSeats], state: StudentState) {
         viewModel = PresetViewModel(columnSeats: columnSeats, state: state)
@@ -33,18 +38,54 @@ struct PresetView: View {
             HStack {
                 ForEach(column.rowSeats, id: \.id) { row in
                     Group{
-                        if row.isOn {
-                            NavigationLink(destination: DetailPresetView(/*id: row.id)*/)) {
-                                Image(systemName: "o.square")
-                                    .foregroundColor(row.isBoy ? .blue : .red)
-                                    .font(.system(size: 40))
+                        if row.number != nil{
+                            Image(systemName: "\(row.number).square")
+                        } else if row.isOn {
+                            if row.isBoy {
+                                Button(action: {
+                                    self.isModal = true
+                                }){
+                                    Image(systemName: "o.square")
+                                        .foregroundColor(.blue)
+                                        .font(.system(size: 40))
+                                }
+                                .sheet(isPresented: self.$isModal){
+                                    DetailPresetView(id: row.id, viewModel: DetailPresetViewModel(columnSeats: self.viewModel.columnSeats, state: self.viewModel.state), studentNumber: self.viewModel.state.boysNumber)
+                                }
+                                
+                            } else {
+                                Button(action: {
+                                    self.isModal = true
+                                }){
+                                    Image(systemName: "o.square")
+                                        .foregroundColor(.red)
+                                        .font(.system(size: 40))
+                                }
+                                .sheet(isPresented: self.$isModal){
+                                    DetailPresetView(id: row.id, viewModel: DetailPresetViewModel(columnSeats: self.viewModel.columnSeats, state: self.viewModel.state), studentNumber: self.viewModel.state.girlsNumber)
+                                }
                             }
+                            //                            if row.isBoy {
+                            //                                NavigationLink(destination: DetailPresetView(id: row.id, viewModel: DetailPresetViewModel(columnSeats: self.viewModel.columnSeats, state: self.viewModel.state), studentNumber: self.viewModel.state.boysNumber)) {
+                            //                                    Image(systemName: "o.square")
+                            //                                        .foregroundColor(row.isBoy ? .blue : .red)
+                            //                                        .font(.system(size: 40))
+                            //                                }
+                            //                            } else {
+                            //                                NavigationLink(destination: DetailPresetView(id: row.id, viewModel: DetailPresetViewModel(columnSeats: self.viewModel.columnSeats, state: self.viewModel.state), studentNumber: self.viewModel.state.girlsNumber)) {
+                            //                                    Image(systemName: "o.square")
+                            //                                        .foregroundColor(row.isBoy ? .blue : .red)
+                            //                                        .font(.system(size: 40))
+                            //                                }
+                            //                            }
                         } else {
                             Image(systemName: "xmark.square")
                                 .font(.system(size: 40))
                         }
                     }
-                    
+                }.onAppear{
+                    print(self.viewModel.state.girlsNumber)
+                    print(self.viewModel.state.boysNumber)
                 }
             }
         }
